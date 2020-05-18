20c3d99c5272bef6eb358272cca538828f872d7e
Daniel Villaverde <j.danielv@outlook.com>
2020-05-18 03:25:20 -0700

    finished MOBILE FIRST mockup 100% - next up are tablet and desktop

--- sprint-one/src/components/MainVideo.js
+++ sprint-one/src/components/MainVideo.js
@@ -3,6 +3,22 @@ import React from "react";
 function MainVideo() {
   return (
     <div className="main-video">
+      <div className="main-video__controls-overlay">
+        <button className="main-video__button main-video__button--play">
+          <i className="main-video__play-icon"></i>
+        </button>
+        <div className="main-video__time-bar">
+          <div className="main-video__time-line"></div>
+        </div>
+        <div className="main-video__fullscreen-volume-buttons">
+          <button className="main-video__button main-video__button--fullscreen">
+            <i className="main-video__fullscreen-icon"></i>
+          </button>
+          <button className="main-video__button main-video__button--volume">
+            <i className="main-video__volume-icon"></i>
+          </button>
+        </div>
+      </div>
       <video
         className="main-video__video"
         poster="./assets/images/video-list-0.jpg"
--- sprint-one/src/components/VideosList.js
+++ sprint-one/src/components/VideosList.js
@@ -2,17 +2,26 @@ import React from "react";
 
 function VideosList(props) {
   return (
-    <ul className="videos-list__ul">
-      {props.videosArray.map((sideVideo) => (
-        <li key={sideVideo.id} className="videos-list__video-wrapper">
-          <div className="videos-list__video-thumbnail" />
-          <div className="video-list__video-details-wrapper">
-            <h4 className="video-list__video-title">{sideVideo.title}</h4>
-            <h5 className="video-list__video-author">{sideVideo.user}</h5>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <div className="videos-list">
+      <h5 className="videos-list__label">Next video</h5>
+      <ul className="videos-list__ul">
+        {props.videosArray.map((sideVideo) => (
+          <li key={sideVideo.id} className="videos-list__video-wrapper">
+            <div className="videos-list__video-thumbnail-container">
+              <img
+                className="videos-list__video-thumbnail"
+                src={sideVideo.thumbnailUrl}
+                alt="next video thumbnail"
+              />
+            </div>
+            <div className="videos-list__video-details-wrapper">
+              <h4 className="videos-list__video-title">{sideVideo.title}</h4>
+              <h5 className="videos-list__video-author">{sideVideo.user}</h5>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 export default VideosList;
