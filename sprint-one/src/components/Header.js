5337b85172876fb52b78ca61dc950a4a9735f566
Daniel Villaverde <j.danielv@outlook.com>
2020-05-18 14:02:13 -0700

    finished header TABLET breakpoint, will continue later with the rest of site

--- sprint-one/src/components/Header.js
+++ sprint-one/src/components/Header.js
@@ -5,20 +5,21 @@ function Header() {
   return (
     <header className="header">
       <img className="header__logo-img" src={logo} alt="brainflix logo" />
+      <div className="header__search-button-img-wrapper">
+        <input
+          className="header__search-bar"
+          type="text"
+          name="search-bar"
+          placeholder="Search"
+        />
 
-      <input
-        className="header__search-bar"
-        type="text"
-        name="search-bar"
-        placeholder="Search"
-      />
-
-      <div className="header__button-img-wrapper">
-        <a className="header__button" href="#">
-          <span className="header__button-plus-sign">+</span>
-          <span className="header__button-text">Upload</span>
-        </a>
-        <div className="header__user-img"></div>
+        <div className="header__button-img-wrapper">
+          <a className="header__button" href="#">
+            <span className="header__button-plus-sign">+</span>
+            <span className="header__button-text">Upload</span>
+          </a>
+          <div className="header__user-img"></div>
+        </div>
       </div>
     </header>
   );
