b53d23528ac852d180932819446457970b874ebe
Daniel Villaverde <j.danielv@outlook.com>
2020-05-19 01:03:25 -0700

    almost done styling all, about to try something crazy so committing first

--- sprint-one/src/components/CommentsSection.js
+++ sprint-one/src/components/CommentsSection.js
@@ -10,17 +10,20 @@ function CommentsSection(props) {
       <div className="comments-section__form-wrapper">
         <div className="comments-section__form-user-img" />
         <form className="comments-section__form">
-          <label
-            className="comments-section__form-label"
-            htmlFor="comment-field"
-          >
-            Join the conversation
-          </label>
-          <textarea
-            type="text"
-            className="comments-section__form-input"
-            name="comment-field"
-          />
+          <div className="comments-section__form-input-wrapper">
+            <label
+              className="comments-section__form-label"
+              htmlFor="comment-field"
+            >
+              Join the conversation
+            </label>
+            <textarea
+              type="text"
+              className="comments-section__form-input"
+              name="comment-field"
+              placeholder="Add a comment"
+            />
+          </div>
           <button type="submit" className="comments-section__form-button">
             Comment
           </button>
