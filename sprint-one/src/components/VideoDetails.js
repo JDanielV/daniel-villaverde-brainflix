1755c37f9b63890c8291826c30ec0b84ff744262
Daniel Villaverde <j.danielv@outlook.com>
2020-05-18 21:40:52 -0700

    fixed a few whoopsies with the video controls overlay design on MOBILE - carrying on with the rest now

--- sprint-one/src/components/VideoDetails.js
+++ sprint-one/src/components/VideoDetails.js
@@ -8,34 +8,36 @@ function VideoDetails(props) {
     <div className="video-details">
       <div className="video-details__title-author-stats-wrapper">
         <h1 className="video-details__title">{props.mainVideoDetails.title}</h1>
-        <div className="video-details__author-date-wrapper">
-          <span className="video-details__author">
-            {props.mainVideoDetails.user}
-          </span>
-          <span className="video-details__date">
-            {props.mainVideoDetails.timestamp}
-          </span>
-        </div>
-        <div className="video-details__stats-wrapper">
-          <div className="video-details__views-wrapper">
-            <img
-              className="video-details__views-icon"
-              src="assets/Icons/PNG/Icon-views.png"
-              alt="views icon"
-            />
-            <span className="video-details__views-count">
-              {props.mainVideoDetails.views}
+        <div className="video-details__author-stats-wrapper">
+          <div className="video-details__author-date-wrapper">
+            <span className="video-details__author">
+              {props.mainVideoDetails.user}
+            </span>
+            <span className="video-details__date">
+              {props.mainVideoDetails.timestamp}
             </span>
           </div>
-          <div className="video-details__likes-wrapper">
-            <img
-              className="video-details__likes-icon"
-              src="assets/Icons/PNG/Icon-likes.png"
-              alt="likes icon"
-            />
-            <span className="video-details__likes-count">
-              {props.mainVideoDetails.likes}
-            </span>
+          <div className="video-details__stats-wrapper">
+            <div className="video-details__views-wrapper">
+              <img
+                className="video-details__views-icon"
+                src="assets/Icons/PNG/Icon-views.png"
+                alt="views icon"
+              />
+              <span className="video-details__views-count">
+                {props.mainVideoDetails.views}
+              </span>
+            </div>
+            <div className="video-details__likes-wrapper">
+              <img
+                className="video-details__likes-icon"
+                src="assets/Icons/PNG/Icon-likes.png"
+                alt="likes icon"
+              />
+              <span className="video-details__likes-count">
+                {props.mainVideoDetails.likes}
+              </span>
+            </div>
           </div>
         </div>
       </div>
