a0dfe63d8c12dd660a5ac932c295a239e8099cd0
Daniel Villaverde <j.danielv@outlook.com>
2020-06-01 22:09:19 -0700

    tested functionality, deleted commented out code

--- sprint-three/client/src/components/CommentsSection.js
+++ sprint-three/client/src/components/CommentsSection.js
@@ -3,26 +3,12 @@ import CommentsList from "./CommentsList";
 
 // Contains the comments form and comments <ul>
 function CommentsSection(props) {
-  //   // Function related to submitComment fn
-  //   // to "throw" the form input value to MainContentWrapper
-  // const onClick = (event) => {
-  //   event.preventDefault();
-  //   const comment = event.target.commentField.value;
-
-  //   props.submitComment(event, comment, props.mainVideoDetails.id);
-  //   event.target.commentField.value = "";
-  // };
-
-  // console.log("props in comments section: ", props);
   return (
     <div className="comments-section">
       <span className="comments-section__comments-count">3 Comments</span>
       <div className="comments-section__form-wrapper">
         <div className="comments-section__form-user-img" />
-        <form
-          className="comments-section__form"
-          // onSubmit={onClick}
-        >
+        <form className="comments-section__form">
           <div className="comments-section__form-input-wrapper">
             <label
               className="comments-section__form-label"
